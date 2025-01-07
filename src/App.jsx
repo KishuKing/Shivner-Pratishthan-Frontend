311fbcc8c21cfa05640b954a007d2e94200c51fb
KishuKing <142623708+KishuKing@users.noreply.github.com>
2025-01-08 00:02:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import AdminDashboard from "./Components/dashboards/AdminDashboard";
+import Home from "./Components/Pages/Home";
+import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+import WhatWeDo from "./Components/Pages/WhatWeDo";
+import DonationForm from "./Components/Pages/DonationForm";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <div className="px-3">
-        <AdminDashboard />
+        {/* If you want to add any Component/Page add it in this div */}
       </div>
+      <Footer />
     </div>
   );
 };
