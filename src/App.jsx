c013c70b4b07165186f5a1f635770be360319d4d
KishuKing <142623708+KishuKing@users.noreply.github.com>
2025-01-08 17:03:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
+import React, { useState } from "react";
 import AdminDashboard from "./Components/dashboards/AdminDashboard";
 import Home from "./Components/Pages/Home";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 import WhatWeDo from "./Components/Pages/WhatWeDo";
 import DonationForm from "./Components/Pages/DonationForm";
+import ContactUs from "./Components/Pages/ContactUs";
+import EventDetails from "./Components/EventDetails";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <div className="px-3">
-        {/* If you want to add any Component/Page add it in this div */}
+    <Router>
+      <div>
+        <Navbar />
+        <div className="px-3">
+          {/* Define the Routes for your application */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/events" element={<WhatWeDo />} />
+            <Route path="/contact" element={<ContactUs />} />
+            <Route path="/donation/donationform" element={<DonationForm />} />
+            <Route path="/events/:id" element={<EventDetails />} />
+            {/* Add more routes here as needed */}
+          </Routes>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Router>
   );
 };
 
