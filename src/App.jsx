dc0a5edf151de1701d435236d28d82c87aa6f3ab
KishuKing <142623708+KishuKing@users.noreply.github.com>
2025-01-13 17:46:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,12 @@ import DonationForm from "./Components/Pages/DonationForm";
 import ContactUs from "./Components/Pages/ContactUs";
 import EventDetails from "./Components/EventDetails";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import VolunteerPage from "./Components/Pages/VolunteerPage ";
+import DonatePage from "./Components/Pages/DonatePage ";
+import Login from "./Components/Pages/Login";
+import Register from "./Components/Pages/Register";
+import StoriesPage from "./Components/Pages/StoriesPage";
+import AboutUs from "./Components/Pages/AboutUs";
 
 const App = () => {
   return (
@@ -20,8 +26,16 @@ const App = () => {
             <Route path="/" element={<Home />} />
             <Route path="/events" element={<WhatWeDo />} />
             <Route path="/contact" element={<ContactUs />} />
+            <Route path="/donation" element={<DonatePage />} />
             <Route path="/donation/donationform" element={<DonationForm />} />
             <Route path="/events/:id" element={<EventDetails />} />
+            <Route path="/admin/admin-dashboard" element={<AdminDashboard />} />
+            <Route path="/stories" element={<StoriesPage />} />
+            <Route path="/aboutus" element={<AboutUs />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            {/* For short time addition */}
+            <Route path="/volunteer" element={<VolunteerPage />} />
             {/* Add more routes here as needed */}
           </Routes>
         </div>
@@ -32,3 +46,4 @@ const App = () => {
 };
 
 export default App;
+
